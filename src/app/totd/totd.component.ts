8aa0836ec31ca4369cc9eebb99653e38d631691c
dev <dev@DEV-TEMP.orion.internal>
2018-10-05 12:02:50 +1300

    Added blank Toast of the Day Component

---
+++ src/app/totd/totd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-totd',
+  templateUrl: './totd.component.html',
+  styleUrls: ['./totd.component.css']
+})
+export class TotdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
